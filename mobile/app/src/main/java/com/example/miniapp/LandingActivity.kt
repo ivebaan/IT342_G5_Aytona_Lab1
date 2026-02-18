02ff89972277b7020f17a74d5463f1f3a0e7957b
rodivanne <rodayban@gmail.com>
2026-02-18 22:45:20 +0800

    feat(landing): addded landing page for mobile dev

---
+++ mobile/app/src/main/java/com/example/miniapp/LandingActivity.kt
@@ -0,0 +1,13 @@
+package com.example.miniapp
+
+import android.app.Activity
+import android.os.Bundle
+
+class LandingActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_landing)
+    }
+}
